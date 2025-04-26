c9b9896655e69b75682c478de542b69f3b5c7170
Prashant Shukla <prashantshukla1029@gmail.com>
2025-04-26 11:12:05 +0530

    navbar is completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import {assets} from '../assets/assets.js'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+    const [visible, setVisible] = React.useState(false);
   return (
     <div className='flex items-center justify-between py-4 font-medium'>
 
@@ -28,6 +29,45 @@ const Navbar = () => {
             </NavLink>
 
         </ul>
+
+        <div className='flex items-center gap-6'>
+
+            <img src={assets.search_icon} alt="" srcset="" className='w-5 cursor-pointer'/>
+
+            <div className='group relative'>
+                <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" srcset="" />
+
+                <div className='group-hover:block hidden dropdown-menu absolute right-0 pt-4'>
+                    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500'>
+                        <p className='cursor-pointer hover:text-black'>My profile</p>
+                        <p className='cursor-pointer hover:text-black'>Orders</p>
+                        <p className='cursor-pointer hover:text-black'>Logout</p>
+                    </div>
+                </div>
+            </div>
+
+            <Link to='/cart' className='relative'>
+                <img className='w-5 min-w-5' src={assets.cart_icon} alt="" srcset="" />
+                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-amber-900 text-white aspect-square rounded-full text-[8px]'>10</p>
+            </Link>
+
+            <img onClick={() => setVisible(true)} className='w-5 sm:hidden curson-pointer' src={assets.menu_icon} alt="" srcset="" />
+        </div>
+
+        {/** sidebar for smaller screens */}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+            <div className='flex flex-col text-gray-600'>
+                <div onClick={()=> setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                    <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" srcset="" />
+                    <p>Back</p>
+                </div>
+                <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+                <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+                <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+                <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+            </div>
+
+        </div>
         
     </div>
   )
